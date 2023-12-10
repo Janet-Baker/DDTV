0c8da9b69de1f6df1313ca119d3fe2d705dc1039
Janet Baker <guoxiangyuan1@gmail.com>
2023-12-11 02:30:58 +0800

    移除更新功能
    我不要你提醒版本更新啊啊啊

--- DDTV_Core/Tool/DDTV_Update.cs
+++ DDTV_Core/Tool/DDTV_Update.cs
@@ -1,12 +1,8 @@
 ﻿using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
-using System.Threading;
-using DDTV_Core.SystemAssembly.Log;
 
 namespace DDTV_Core.Tool
 {
@@ -38,96 +34,6 @@ namespace DDTV_Core.Tool
             catch (Exception)
             {
                 return false;
-            }
-        }
-        public static bool IsOn = false;
-        /// <summary>
-        /// 检查更新程序是否是最新的
-        /// </summary>
-        public static void CheckUpdateProgram(bool IsUp=false)
-        {
-            if (IsUp)
-            {
-                Task.Run(() =>
-                {
-                    try
-                    {
-                        Log.AddLog("Check", LogClass.LogType.Info, "开始手动更新更新器", false, null, false);
-                        CheckTheUpdateProgramForUpdates();
-                        Log.AddLog("Check", LogClass.LogType.Info, "手动更新更新器OK", false, null, false);
-                    }
-                    catch (Exception){}
-                });
-            }
-            else
-            {
-                Task.Run(() =>
-                {
-                    if (!IsOn)
-                    {
-                        while (true)
-                        {
-                            IsOn=true;
-                            try
-                            {
-                                CheckTheUpdateProgramForUpdates();
-                                Thread.Sleep(10800 * 1000);
-                            }
-                            catch (Exception)
-                            {
-                                Thread.Sleep(60 * 1000);
-                            }
-                        }
-                    }
-                });
-            }
-        }
-        /// <summary>
-        /// 检查更新程序是否是最新的
-        /// </summary>
-        public static void CheckTheUpdateProgramForUpdates()
-        {
-            try
-            {    
-                string Type = "Update";
-                string PushUrl = "https://update.ddtv.pro/";
-                string _A = SystemAssembly.NetworkRequestModule.Get.Get.GetRequest(PushUrl + $"{Type}_Update.json", false, "https://Update.ddtv.pro/");
-                FileInfoClass OldUpdateInfo = JsonConvert.DeserializeObject<FileInfoClass>(_A);
-                FileInfoClass files = new FileInfoClass()
-                {
-                    Bucket = OldUpdateInfo.Bucket,
-                    Description = OldUpdateInfo.Description,
-                    Type = Type,
-                    Ver = OldUpdateInfo.Ver,
-                };
-                foreach (var item in OldUpdateInfo.files)
-                {
-                    if (File.Exists(item.FilePath))
-                    {
-                        if (item.FileMd5 != SystemAssembly.EncryptionModule.Encryption.GetMD5HashFromFile(item.FilePath))
-                        {
-                            files.files.Add(item);
-                        }
-                    }
-                    else
-                    {
-                        files.files.Add(item);
-                    }
-                }
-                foreach (var file in files.files)
-                {
-                    string DownloadUrl = PushUrl + Type + "/" + file.FilePath;
-
-                    //Console.WriteLine($"正在下载大小{file.Size}字节的文件:{file.FilePath}");
-                    Log.AddLog("DDTV_Update", LogClass.LogType.Debug, $"检测到更新器文件变化，开始下载文件[{file.FilePath}({SystemAssembly.NetworkRequestModule.NetClass.ConversionSize(file.Size)})]", false, null, false);
-                    SystemAssembly.NetworkRequestModule.Get.Get.GetFile_For_Update(DownloadUrl, file.FilePath);
-                    Log.AddLog("DDTV_Update", LogClass.LogType.Debug, $"下载文件[{file.FilePath}]完成", false, null, false);
-                }
-               
-            }
-            catch (Exception)
-            {
-               
             }
         }
 
--- DDTV_GUI/Tool/ServerInteraction.cs
+++ DDTV_GUI/Tool/ServerInteraction.cs
@@ -14,63 +14,11 @@ namespace DDTV_GUI.Tool
     {
         public static void Start()
         {
-            CheckUpdates.Update();
             Notice.Start();
             //Dokidoki.Start();
             
         }
-        public class CheckUpdates
-        {
-            public static event EventHandler<EventArgs> NewUpdate;
-           
-            /// <summary>
-            /// 间隔时间(ms)
-            /// </summary>
-            public static int ReminderInterval = 60 * 60 * 1 * 1000;
-            private static bool IsChecked = false;
-            private static bool Is = false;
-            public static void Update()
-            {
-                if (!Is)
-                {
-                    Task.Run(() =>
-                    {
-                        Is = true;
-                        while (true)
-                        {
-                            try
-                            {
-                                if (IsChecked)
-                                {
-                                    Thread.Sleep(ReminderInterval);
-                                    if (NewUpdate != null)
-                                    {
-                                        NewUpdate.Invoke(null, EventArgs.Empty);
-                                    }
-                                    Console.WriteLine("检测到新版本，请退出本程序后运行目录中的[DDTV_Update]进行更新\n");
-                                }
-                                else
-                                {
-                                    if (DDTV_Core.Tool.DDTV_Update.ComparisonVersion("GUI", System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString()))
-                                    {
-                                        IsChecked = true;
-                                        if (NewUpdate != null)
-                                        {
-                                            NewUpdate.Invoke(null, EventArgs.Empty);
-                                        }
-                                    }
-                                    
-                                }
-                            }
-                            catch (Exception)
-                            {
-                            }
-                            Thread.Sleep(60 * 30 * 1000);
-                        }
-                    });
-                }
-            }
-        }
+
         public class Notice
         {
             public static event EventHandler<EventArgs> NewNotice;

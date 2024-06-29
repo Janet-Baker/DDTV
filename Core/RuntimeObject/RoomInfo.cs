4749f1ba016c8725b247bf5f7193b6aafacac09b
Janet Baker <guoxiangyuan1@gmail.com>
2024-06-29 21:07:32 +0800

    修改传webhook细节
    多传一点数据给我嘛

--- Core/RuntimeObject/RoomInfo.cs
+++ Core/RuntimeObject/RoomInfo.cs
@@ -1487,7 +1487,6 @@ namespace Core.RuntimeObject
         /// 直播间分区id
         /// </summary>
         public ExpansionType<int> area = new() { ExpirationTime = DateTime.UnixEpoch, Value = -1 };
-        [JsonIgnore]
         /// <summary>
         /// 直播间分区名
         /// </summary>
@@ -1497,12 +1496,10 @@ namespace Core.RuntimeObject
         /// 直播间新版分区id
         /// </summary>
         public ExpansionType<int> area_v2_id = new() { ExpirationTime = DateTime.UnixEpoch, Value = -1 };
-        [JsonIgnore]
         /// <summary>
         /// 直播间新版分区名
         /// </summary>
         public ExpansionType<string> area_v2_name = new() { ExpirationTime = DateTime.UnixEpoch, Value = string.Empty };
-        [JsonIgnore]
         /// <summary>
         /// 直播间父分区名
         /// </summary>
@@ -1512,7 +1509,6 @@ namespace Core.RuntimeObject
         /// 直播间父分区id
         /// </summary>
         public ExpansionType<int> area_v2_parent_id = new() { ExpirationTime = DateTime.UnixEpoch, Value = -1 };
-        [JsonIgnore]
         /// <summary>
         /// 主播头像url
         /// </summary>
@@ -1535,7 +1531,6 @@ namespace Core.RuntimeObject
         /// 直播关键帧图
         /// </summary>
         public ExpansionType<string> keyframe = new() { ExpirationTime = DateTime.UnixEpoch, Value = string.Empty };
-        [JsonIgnore]
         /// <summary>
         /// 直播间锁定时间戳
         /// </summary>
@@ -1560,7 +1555,6 @@ namespace Core.RuntimeObject
         /// 是否隐藏
         /// </summary>
         public ExpansionType<bool> is_hidden = new() { ExpirationTime = DateTime.UnixEpoch, Value = false };
-        [JsonIgnore]
         /// <summary>
         /// 是否锁定
         /// </summary>
